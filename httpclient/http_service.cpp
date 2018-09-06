bffb30e9df6fec4b2d255088c0d3bedbb9712fe1
Excited Spider <213161557@seu.edu.cn>
2018-09-06 12:46:14 +0800

    httpclient

---
+++ httpclient/http_service.cpp
@@ -0,0 +1,165 @@
+#include "http_service.h"
+#define SERVER_IP "127.0.0.1"
+
+HttpService* HttpService::instance = nullptr;
+
+HttpService::HttpService():is_gc_setted(false), is_jr_setted(false)
+{
+}
+
+HttpService * HttpService::getInstance()
+{
+	if (HttpService::instance == nullptr)
+	{
+		HttpService::instance = new HttpService();
+	}
+	return instance;
+
+}
+
+void HttpService::joinRoom(std::string ipv4)
+{
+	HttpRequest* request = new HttpRequest();
+	request->setRequestType(HttpRequest::Type::POST);
+	std::stringstream ss;
+	ss << "http:/SERVER_IP:8080/join_room?ipv4=" << ipv4;
+	request->setUrl(ss.str());
+	request->setResponseCallback(boost::bind(&HttpService::joinroom_callback, this, _1,_2));
+	request->setTag("Join Room");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpService::openRoom(std::string ipv4)
+{
+	HttpRequest* request = new HttpRequest();
+	request->setRequestType(HttpRequest::Type::POST);
+	std::stringstream ss;
+	ss << "http://"<< SERVER_IP <<":8080/open_room?ipv4=" << ipv4;
+	request->setUrl(ss.str());
+	request->setResponseCallback(boost::bind(&HttpService::joinroom_callback, this, _1, _2));
+	request->setTag("Open Rooms");
+	HttpClient::getInstance()->send(request);
+	request->release();
+
+}
+
+void HttpService::getRoomsViaHttp()
+{
+	HttpRequest* request = new HttpRequest();
+	request->setRequestType(HttpRequest::Type::GET);
+	request->setUrl("http://SERVER_IP:8080/get_rooms");
+	request->setResponseCallback(boost::bind(&HttpService::getroom_callback,this,_1,_2));
+	request->setTag("Get Rooms");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpService::set_getrooms_cb(boost::function<void(room_list_type*)> callback)
+{
+	is_gc_setted = true;
+	m_getrooms_callback = callback;
+}
+
+void HttpService::set_joinroom_cb(boost::function<void(bool)> callback)
+{
+	is_jr_setted = true;
+	m_getrooms_callback = callback;
+}
+
+room_list_type * HttpService::parseRoomListJson(const char * json)
+{
+	rapidjson::Document d;
+	d.Parse<0>(json);
+	if (d.HasParseError())
+	{
+		cocos2d::log("parse Json error");
+		return new room_list_type();
+	}
+	if (d.IsObject() && d.HasMember("roomAmount") && d.HasMember("errorCode") && d.HasMember("roomlist"))
+	{
+		if (d["errorCode"].GetInt())
+		{
+			cocos2d::log("server error");
+			return new room_list_type();
+		}
+		else
+		{
+			auto rl = new room_list_type();
+			int roomAmount = d["roomAmount"].GetInt();
+			const rapidjson::Value& roomArray = d["roomlist"];
+			assert(roomArray.IsArray());
+			for (auto i = 0; i != roomAmount; i++)
+			{
+				auto object = roomArray[i].GetObjectW();
+				auto room = new st_room;
+				room->ipv4 = object["ipv4"].GetString();
+				room->playernum = object["playerNum"].GetInt();
+				rl->push_back(room_ptr_type(room));
+			}
+			return rl;
+		}
+	}
+}
+
+const room_ptr_type HttpService::parseRoomJson(const char * json)
+{
+	rapidjson::Document d;
+	d.Parse<0>(json);
+	if (d.HasParseError())
+	{
+		cocos2d::log("parse Json error");
+		return room_ptr_type();
+	}
+	if (d.IsObject() && d.HasMember("ipv4") && d.HasMember("playerNum"))
+	{
+		auto room = new st_room();
+		room->ipv4 = d["ipv4"].GetString();
+		room->playernum = d["playerNum"].GetInt();
+		return room_ptr_type(room);
+	}
+	cocos2d::log("parse Json error");
+	return room_ptr_type();
+
+}
+
+void HttpService::getroom_callback(HttpClient * sender, HttpResponse * response)
+{
+	std::stringstream ss;
+	auto charvec = response->getResponseData();
+	for (int i = 0; i < charvec->size(); i++)
+	{
+		ss << charvec->at(i);
+	}
+	room_list_type *rl = parseRoomListJson(ss.str().c_str());
+	m_cached_list = rl;
+	if (is_gc_setted)
+	{
+		m_getrooms_callback(m_cached_list);
+	}
+	else
+	{
+		cocos2d::log("have not set get rooms callback in httpservice!");
+	}
+}
+
+void HttpService::joinroom_callback(HttpClient * sender, HttpResponse * response)
+{
+	std::stringstream ss;
+	auto charvec = response->getResponseData();
+	for (int i = 0; i < charvec->size(); i++)
+	{
+		ss << charvec->at(i);
+	}
+	bool join_succeed = false;
+	if (ss.str() == "true")
+		join_succeed = true;
+
+	cocos2d::log(ss.str().data());
+	if(is_jr_setted)
+		m_join_room_callback(join_succeed);
+	else
+	{
+		cocos2d::log("have not set join room callback in httpservice!");
+	}
+}
---
+++ httpclient/http_service.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "network/HttpClient.h"
+#include "cocos2d.h"
+#include <sstream>
+#include "json/rapidjson.h"
+#include "json/document.h"
+#include "boost/function.hpp"
+#include "st_Room.h"
+#include "boost/smart_ptr.hpp"
+#include <vector>
+#include "boost/bind.hpp"
+
+using namespace cocos2d::network;
+
+typedef std::vector<boost::shared_ptr<st_room>> room_list_type;
+typedef boost::shared_ptr<st_room> room_ptr_type;
+
+class HttpService
+{
+private:
+	room_list_type *m_cached_list;
+	boost::function<void(room_list_type *)> m_getrooms_callback;
+	bool is_gc_setted;
+	boost::function<void(bool)> m_join_room_callback;
+	bool is_jr_setted;
+	static HttpService* instance;
+public:
+	static HttpService* getInstance();
+	void joinRoom(std::string ipv4);
+	void openRoom(std::string ipv4);
+	void getRoomsViaHttp();
+	void set_getrooms_cb(boost::function<void(room_list_type *)> callback);
+	void set_joinroom_cb(boost::function<void(bool)> callback);
+private:
+	HttpService();
+	room_list_type * parseRoomListJson(const char * json);
+	const room_ptr_type parseRoomJson(const char * json);
+	void getroom_callback(HttpClient *sender, HttpResponse *response);
+	void joinroom_callback(HttpClient *sender, HttpResponse *response);
+};
